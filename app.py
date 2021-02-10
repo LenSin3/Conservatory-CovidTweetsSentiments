667ad767658f97a3b25f182bb90190380af7203e
LenSin3 <lenpk3@yahoo.co.uk>
2021-02-09 20:17:39 -0800

    created end points to plot graphs

--- app.py
+++ app.py
@@ -7,12 +7,15 @@ import pickle
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.svm import LinearSVC
-
 from config import DATABASE_URI
+
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
+import numpy as np
+import pandas as pd
+import datetime
 
 # import and load saved transformer and model
 
@@ -31,22 +34,14 @@ with open(model_path, "rb") as f:
 
 
 
-
-
 app = Flask(__name__)
 
-# Database setup
+#################################################
+# Database Setup
+#################################################
 
-app.config(DATABASE_URI)
-db = SQLAlchemy(app)
-
-# reflect an existing database into a new model
-Base = automap_base()
-# reflect the tables
-Base.prepare(db.engine, reflect=True)
-
-# Save references to each table
-tweetsRatings = Base.classes.tweets_sentiment
+engine = create_engine(DATABASE_URI)
+conn = engine.connect()
 
 
 # create route that renders index.html template with prediction app
@@ -72,10 +67,63 @@ def index():
 # create route to return unique dates
 @app.route("/unique_dates")
 def unique_dates():
+    
+
     """Returns a list of unique dates"""
-    udates = db.session.query(tweetsRatings.tweeet_date).distinct().all()
-    return jsonify(udates)
-	
+    df_dates = pd.read_sql_query('''SELECT DISTINCT(tweeet_date) from tweets_sentiment;''', conn)
+    df_dates['dates'] = pd.to_datetime(df_dates['tweeet_date'], errors='coerce')
+    df_dates['month'] = df_dates['dates'].dt.strftime('%b')
+    df_dates_unq = df_dates['month'].unique()
+    
+    # Convert list of tuples into normal list
+    # unique_dates = list(np.ravel(udates))
+    return jsonify(list(df_dates_unq))
+
+# create route to return sentiments
+@app.route("/getlabels/<label>")
+def getLabels(label):
+
+    """Returns sentiment counts by date"""
+    df_sent = pd.read_sql_query("""SELECT tweeet_date, sentiment FROM tweets_sentiment;""", conn)
+    dfMain = df_sent.loc[df_sent['sentiment'] == label.title()]
+    dfMain['dates'] = pd.to_datetime(dfMain['tweeet_date'], errors='coerce')
+    dfMain['month'] = dfMain['dates'].dt.strftime('%b')
+    dfLabel = dfMain.groupby('month').count().reset_index()
+    
+    
+    
+
+    label_count = []
+
+    for index, row in dfLabel.iterrows():
+        dfLabels = {}
+        dfLabels['month'] = row['month']
+        dfLabels['sentimentcount'] = row['sentiment']
+        label_count.append(dfLabels)
+        
+    return jsonify(label_count)
+# create route to filter by label and month
+@app.route('/getmonthlabels/<monthlab>')
+def getLabelsbyMonth(monthlab):
+
+    """Returns Count of Labels in that month"""
+    df_sent = pd.read_sql_query("""SELECT tweeet_date, tweet, sentiment FROM tweets_sentiment;""", conn)
+    
+    df_sent['dates'] = pd.to_datetime(df_sent['tweeet_date'], errors='coerce')
+    df_sent['month'] = df_sent['dates'].dt.strftime('%b')
+    dfMain = df_sent.loc[df_sent['month'] == str(monthlab.title())]
+    dfLabel = dfMain.groupby('sentiment')['tweet'].count().reset_index()
+
+    label_count = []
+
+    for index, row in dfLabel.iterrows():
+        dfLabels = {}
+        dfLabels['sentiment'] = row['sentiment']
+        dfLabels['tweetCount'] = row['tweet']
+        label_count.append(dfLabels)
+        
+    return jsonify(label_count)
+
 
 if __name__ == "__main__":
 
